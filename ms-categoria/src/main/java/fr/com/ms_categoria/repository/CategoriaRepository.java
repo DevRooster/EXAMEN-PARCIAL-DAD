da81b2e7316dcecc2e57115cffd969a25e7a859f
23Vvrangler <leninbk17@gmail.com>
2024-11-30 13:10:44 -0500

    Refresh

--- ms-categoria/src/main/java/fr/com/ms_categoria/repository/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.com.ms_categoria.repository;
-
-
-import fr.com.ms_categoria.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository <Categoria,Integer> {
-}
--- ms-producto/src/main/java/fr/com/ms_producto/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.com.ms_producto.repository;
-
-
-import fr.com.ms_producto.entity.Producto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository <Producto,Integer> {
-}
